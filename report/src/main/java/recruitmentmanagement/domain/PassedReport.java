54128a70139387fa81db47ea1fc8fd14369c45bd
kyusooK <rbtn110@uengine.org>
2025-03-12 14:25:36 +0000

    c

--- report/src/main/java/recruitmentmanagement/domain/PassedReport.java
+++ report/src/main/java/recruitmentmanagement/domain/PassedReport.java
@@ -13,7 +13,7 @@ import lombok.Data;
 public class PassedReport {
 
     @Id
-    //@GeneratedValue(strategy=GenerationType.AUTO)
+    @GeneratedValue(strategy=GenerationType.AUTO)
     private Long id;
 
     private String name;
